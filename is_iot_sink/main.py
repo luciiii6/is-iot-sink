2d235c76b810981ecf14518de1691ae974b2f07e
Daniel Popa <danypdaniel@gmail.com>
2022-09-24 02:35:37 +0300

    Fix main cancel signal

--- is_iot_sink/main.py
+++ is_iot_sink/main.py
@@ -1,14 +1,21 @@
 from is_iot_sink.sink import Sink
+from is_iot_sink.logger import LOG
 import time
 import sys
+import signal
 
 sink = Sink()
 
+def signal_handler(sig, frame):
+    LOG.info("SIGINT received!")
+    sink.stop()
+    sys.exit(0)
+
 def main():
+    signal.signal(signal.SIGINT, signal_handler)
     sink.start()
     while sink.status() == True:
         time.sleep(1)
-    sys.exit(0)
-
+    
 if __name__ == "__main__":
     main()
