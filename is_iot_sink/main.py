29c53963e7acd968454283f7e870bc1cf58825c9
Daniel Popa <danypdaniel@gmail.com>
2022-09-26 02:16:08 +0300

    Set env variables before starting the application

--- is_iot_sink/main.py
+++ is_iot_sink/main.py
@@ -1,10 +1,18 @@
 from is_iot_sink.sink import Sink
 from is_iot_sink.logger import LOG
+from pathlib import Path
+from dotenv import Dotenv
+import os
 import time
 import sys
 import signal
 
 sink = Sink()
+
+def set_env_variables():
+    env_file_path = Path(os.getenv('PROJECT_PATH') + '/.env')
+    dotenv = Dotenv(env_file_path)
+    os.environ.update(dotenv)
 
 def signal_handler(sig, frame):
     LOG.info("SIGINT received!")
@@ -13,6 +21,7 @@ def signal_handler(sig, frame):
 
 def main():
     signal.signal(signal.SIGINT, signal_handler)
+    set_env_variables()
     sink.start()
     while sink.status() == True:
         time.sleep(1)
