60310ad6720bfb67d51a074c02c585c9b36201d4
Daniel Popa <danypdaniel@gmail.com>
2022-09-27 00:21:14 +0300

    Add info log for configuration file loading

--- is_iot_sink/settings.py
+++ is_iot_sink/settings.py
@@ -1,12 +1,15 @@
 from threading import Lock
+from is_iot_sink.logger import LOG
 import yaml
 import dpath.util
 import os
+import logging
 
 DEFAULT_PATH = os.getenv('PROJECT_PATH') + '/setup.yml'
 
 class Settings:
     def __init__(self, filepath = DEFAULT_PATH, mutex = Lock()):
+        LOG.info(f"Using configuration file: {filepath}")
         file = open(filepath)
         self.__settings = yaml.safe_load(file)
         self.__mutex = mutex
