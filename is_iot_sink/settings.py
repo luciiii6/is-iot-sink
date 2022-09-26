e74ae246212648ca8978f4c8f72bf382ed14a64e
Daniel Popa <danypdaniel@gmail.com>
2022-09-26 21:00:03 +0300

    Refactor mutex using

--- is_iot_sink/settings.py
+++ is_iot_sink/settings.py
@@ -12,13 +12,11 @@ class Settings:
         self.__mutex = mutex
 
     def get(self, name):
-        self.__mutex.acquire()
-        value = dpath.util.get(self.__settings, name)
-        self.__mutex.release()
+        with self.__mutex:
+            value = dpath.util.get(self.__settings, name)
 
         return value
 
     def set(self, name, value):
-        self.__mutex.acquire()
-        dpath.util.set(self.__settings, name, value)
-        self.__mutex.release()
+        with self.__mutex:
+            dpath.util.set(self.__settings, name, value)
