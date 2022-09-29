3d8a8ec0b30c0c9d39391a53a5cba5008f95d3f1
DariusC <darius.cracea@wenglor.com>
2022-09-29 17:16:31 +0200

    removed aliases

--- sink_setup.sh
+++ sink_setup.sh
@@ -2,10 +2,9 @@
 sudo touch /usr/local/bin/startup.sh
 sudo chmod u+x /usr/local/bin/startup.sh
 sudo echo "#!/bin/bash" | sudo tee -a /usr/local/bin/startup.sh
-sudo echo 'shopt -s expand_aliases' | sudo tee -a /usr/local/bin/startup.sh
-sudo echo "alias sink-run='sink-setup-env && python is_iot_sink/main.py'" | sudo tee -a /usr/local/bin/startup.sh
-sudo echo "alias sink-setup-env='cd /home/pi/is-iot-sink && source env/bin/activate && set -o allexport; source .env; set +o allexport'" | sudo tee -a /usr/local/bin/startup.sh
-sudo echo "sink-run" | sudo tee -a /usr/local/bin/startup.sh
+sudo echo "cd /home/pi/is-iot-sink" | sudo tee -a /usr/local/bin/startup.sh
+sudo echo "source env/bin/activate" | sudo tee -a /usr/local/bin/startup.sh
+sudo echo "python is_iot_sink/main.py" | sudo tee -a /usr/local/bin/startup.sh
 #copy the service file to be started by systemd
 sudo cp sink.service /etc/systemd/system/
 sudo systemctl daemon-reload
