ed58bb1af3cf0d009593c5044953b34ee4643786
DariusC <darius.cracea@wenglor.com>
2022-09-29 14:43:05 +0200

    fixed bug and added alias for sink-run

--- sink_setup.sh
+++ sink_setup.sh
@@ -2,3 +2,5 @@
 sudo cp sink.service /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl enable sink.service
+alias sink-run='sink-setup-env && python is_iot_sink/main.py'
+alias sink-setup-env='cd /home/pi/is-iot-sink && source env/bin/activate && set -o allexport; source .env; set +o allexport'
