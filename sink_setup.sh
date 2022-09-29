7d6788d8e8d9fbed87e2b65c750cb02d1121cc2d
DariusC <darius.cracea@wenglor.com>
2022-09-29 17:50:41 +0200

    added missing export paths

--- sink_setup.sh
+++ sink_setup.sh
@@ -4,6 +4,8 @@ sudo chmod u+x /usr/local/bin/startup.sh
 sudo echo "#!/bin/bash" | sudo tee -a /usr/local/bin/startup.sh
 sudo echo "cd /home/pi/is-iot-sink" | sudo tee -a /usr/local/bin/startup.sh
 sudo echo "source env/bin/activate" | sudo tee -a /usr/local/bin/startup.sh
+sudo echo "export PYTHONPATH=$(pwd):${PYTHONPATH}" | sudo tee -a /usr/local/bin/startup.sh
+sudo echo "export PROJECT_PATH=$(pwd)" | sudo tee -a /usr/local/bin/startup.sh
 sudo echo "python is_iot_sink/main.py" | sudo tee -a /usr/local/bin/startup.sh
 #copy the service file to be started by systemd
 sudo cp sink.service /etc/systemd/system/
