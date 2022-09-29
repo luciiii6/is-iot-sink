e9310bfd22477c5a81a56c60963ab748cc21f68e
DariusC <darius.cracea@wenglor.com>
2022-09-29 16:53:06 +0200

    added expand_aliases

--- sink_setup.sh
+++ sink_setup.sh
@@ -2,8 +2,9 @@
 sudo touch /usr/local/bin/startup.sh
 sudo chmod u+x /usr/local/bin/startup.sh
 sudo echo "#!/bin/bash" | sudo tee -a /usr/local/bin/startup.sh
-sudo echo "function sink-setup-env {cd /home/pi/is-iot-sink && source env/bin/activate && set -o allexport; source .env; set +o allexport}" | sudo tee -a /usr/local/bin/startup.sh
-sudo echo "function sink-run {sink-setup-env && python is_iot_sink/main.py}" | sudo tee -a /usr/local/bin/startup.sh
+sudo echo 'shopt -s expand_aliases' | sudo tee -a /usr/local/bin/startup.sh
+sudo echo "alias sink-run='sink-setup-env && python is_iot_sink/main.py'" | sudo tee -a /usr/local/bin/startup.sh
+sudo echo "alias sink-setup-env='cd /home/pi/is-iot-sink && source env/bin/activate && set -o allexport; source .env; set +o allexport'" | sudo tee -a /usr/local/bin/startup.sh
 sudo echo "sink-run" | sudo tee -a /usr/local/bin/startup.sh
 #copy the service file to be started by systemd
 sudo cp sink.service /etc/systemd/system/
